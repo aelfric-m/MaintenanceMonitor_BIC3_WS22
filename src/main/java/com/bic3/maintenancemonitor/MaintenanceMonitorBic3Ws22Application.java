62d39dbe5be19f27e81478812216181fb1673a44
Aelfric <aelfric.mayer@gmx.at>
2022-12-03 10:47:56 +0100

    Initial commit

---
+++ src/main/java/com/bic3/maintenancemonitor/MaintenanceMonitorBic3Ws22Application.java
@@ -0,0 +1,13 @@
+package com.bic3.maintenancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorBic3Ws22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceMonitorBic3Ws22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bic3/maintenancemonitor/MaintenanceMonitorBic3Ws22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bic3.maintenancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorBic3Ws22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
