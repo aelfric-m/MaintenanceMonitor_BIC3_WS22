cdb331ca0d74b6deeb28103508b9fdf1c772ed6f
Johanna <johannagd96@gmail.com>
2022-12-17 17:30:34 +0100

    reformatting code

--- src/main/java/com/bic3/maintenancemonitor/MaintenanceController.java
+++ src/main/java/com/bic3/maintenancemonitor/MaintenanceController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MaintenanceController {
 
- private String defaultStatus = "Everything works as expected, nothing to worry, humans";
- private String currentStatus = "Everything works as expected, nothing to worry, humans";
+    private String defaultStatus = "Everything works as expected, nothing to worry, humans";
+    private String currentStatus = "Everything works as expected, nothing to worry, humans";
 
- @GetMapping("/api/message")
- public String displayCurrentStatus() {
-  return currentStatus;
- }
+    @GetMapping("/api/message")
+    public String displayCurrentStatus() {
+        return currentStatus;
+    }
 
 
 }
