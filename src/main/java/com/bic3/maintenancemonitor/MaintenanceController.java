aa7b23ced3b409206a948d2424fd79eb03e13a68
Johanna <johannagd96@gmail.com>
2022-12-17 17:15:20 +0100

    create MaintenanceController.java add displayCurrentStatus method

---
+++ src/main/java/com/bic3/maintenancemonitor/MaintenanceController.java
@@ -0,0 +1,18 @@
+package com.bic3.maintenancemonitor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MaintenanceController {
+
+ private String defaultStatus = "Everything works as expected, nothing to worry, humans";
+ private String currentStatus = "Everything works as expected, nothing to worry, humans";
+
+ @GetMapping("/api/message")
+ public String displayCurrentStatus() {
+  return currentStatus;
+ }
+
+
+}
